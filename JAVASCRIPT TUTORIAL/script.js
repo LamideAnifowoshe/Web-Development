11241d05cdf07597ffae9252046f197b4615ad21
LamideAnifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2021-12-07 20:12:40 +0100

    Javascript practice

---
+++ JAVASCRIPT TUTORIAL/script.js
@@ -0,0 +1,88 @@
+var firstName = "Lammy";
+var gender;
+console.log(gender);
+console.log(firstName);
+
+var lastName = "Anifowoshe";
+var age = 24;
+var fullAge = true;
+console.log(fullAge);
+
+/**********
+ * Variable Mutation and Type Coerction
+ */
+var firstName = "Lammy";
+var age = 24;
+//Type coertion
+console.log(firstName + " " + age);
+
+var job, isMarried;
+job = "Developer";
+isMarried = false;
+
+console.log(
+  firstName +
+    " is a " +
+    age +
+    " year old " +
+    job +
+    ". Is she married? " +
+    isMarried
+);
+
+//Variable Mutation
+age = "twenty four";
+job = "FrontEnd Web Developer";
+alert(
+  firstName +
+    " is a " +
+    age +
+    " year old " +
+    job +
+    ". Is she married? " +
+    isMarried
+);
+
+var lastName = prompt("what is her lastName?");
+console.log(firstName + " " + lastName);
+
+/*********************
+ * Basic operators
+ */
+var year = 2021;
+var ageLammy = 24;
+var ageBlessing = 25;
+
+var yearBlessing = year - ageBlessing;
+console.log(yearBlessing);
+
+var yearLammy = year - ageLammy;
+console.log(yearLammy);
+
+//Logical Operators
+var lammyOlder = ageLammy > ageBlessing;
+console.log(lammyOlder);
+
+//*typeof operator
+console.log(typeof lammyOlder);
+console.log(typeof ageLammy);
+var x;
+console.log(typeof x);
+console.log(typeof y);
+
+/**************
+ * Coding Challenge
+ */
+var lammyMass = 60;
+var lammyHeight = 1.65;
+var blessingMass = 65;
+var blessingHeight = 1.7;
+
+lammyBMI = lammyMass / (lammyHeight * lammyHeight);
+console.log(lammyBMI);
+
+blessingBMI = blessingMass / (blessingHeight * blessingHeight);
+console.log(blessingBMI);
+
+var lammyHigherBMI = lammyBMI > blessingBMI;
+console.log("is lammy's BMI higher than blessings'?" + lammyHigherBMI);
